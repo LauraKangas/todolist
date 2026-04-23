54d839eef138921ffe16ab2facf8bb2b96aef650
Laura Kangas <laura.kangas@tuni.fi>
2026-04-23 16:14:59 +0300

    Lisätty Clear-painike ja päivitetty testit

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import { expect, test } from 'vitest';
-import '@testing-library/jest-dom/vitest'; // Tämä korvaa matchers-tuonnin ja extendin
+import '@testing-library/jest-dom/vitest';
 import App from './App';
 
 test('renders App component', () => {
@@ -9,7 +9,7 @@ test('renders App component', () => {
   expect(header).toBeInTheDocument();
 });
 
-test('add todo', () => {
+test('add todo and clear', () => {
   render(<App />);
 
   const desc = screen.getByPlaceholderText('Description');
@@ -18,9 +18,14 @@ test('add todo', () => {
   fireEvent.change(date, { target: { value: '29.12.2023' } });
   const status = screen.getByPlaceholderText('Status');
   fireEvent.change(status, { target: { value: 'Open' } });
-  const button = screen.getByText('Add');
-  fireEvent.click(button);
+  
+  const addBtn = screen.getByText('Add');
+  fireEvent.click(addBtn);
 
   const table = screen.getByRole('table');
   expect(table).toHaveTextContent('Go to coffee');
+
+  const clearBtn = screen.getByText('Clear');
+  fireEvent.click(clearBtn);
+  expect(screen.queryByText('Go to coffee')).not.toBeInTheDocument();
 });
